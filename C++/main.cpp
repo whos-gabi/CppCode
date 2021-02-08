2d7fd0039d5d9a562ac11f6acb64f826d729c55f
theCYBERbro <theCYBERbro@users.noreply.github.com>
2021-02-08 13:00:15 +0200

    👀🤪🐜 some code

--- C++/main.cpp
+++ C++/main.cpp
@@ -6,17 +6,42 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//d
-char s[251]="texttestidkitssus", ch='t';
 
 /*
-Să se scrie o funcție C++ care să 
-returneze oglinditul unui număr natural transmis ca parametru.
-Restricţii şi precizări
-numele funcției va fi oglindit
+La un depozit exista n produse pentru fiecare produs se cunoaste:
+codul, denumirea, cantitatea si pretul.
+Sa se calculeze valarea toatala a produselor din depozit
 */
 
+struct depozit{
+    long cod;
+    int cantitate, pret;
+    char denumire[41]; 
+};
+
+depozit v[1001];
+int n,nr;
+
+void citire(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>v[i].cod;
+        cin>>v[i].cantitate;
+        cin>>v[i].pret;
+        cin.get();
+        cin.get(v[i].denumire,41);
+    }
+}
+
+void valTot(int &nr, depozit v[1001], int n){
+    nr=0;
+    for(int j=0;j<n;j++)
+        nr+=(v[j].pret*v[j].cantitate);
+}
+
 int main(){
-    //test 
+    citire();
+    valTot(nr,v,n);
+    cout<<"Valoarea totala = "<<nr;
     return 0;
 }
--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
-    // string FstM,ScndM,Drink;
-    // int n;
-    // cout<<"Enter your order:\n";
-    // cout<<"First enter your first meal name:\n";
-    // cin>>FstM;
-    // cout<<"Enter your second meal name:\n";
-    // cin>>ScndM;
+nr
