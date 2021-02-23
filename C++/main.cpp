9f33e104d32d967efc3f70ccc9b36a18d2ed0eaf
theCYBERbro <theCYBERbro@users.noreply.github.com>
2021-02-23 15:35:02 +0200

    💀update

--- C++/main.cpp
+++ C++/main.cpp
@@ -8,40 +8,74 @@
 using namespace std;
 
 /*
-La un depozit exista n produse pentru fiecare produs se cunoaste:
-codul, denumirea, cantitatea si pretul.
-Sa se calculeze valarea toatala a produselor din depozit
+La un depozit de materiale exista n produse.
+ Pentru fiecare produs se cunoaste:
+  nume_produs, cantitate, pret.
+Se cere: a)valoarea pentru fiecare produs
+         b)cantitatea pe tipuri de produse
+
+Ex: n=5
+  Ulei    10   40
+  Zahar   5    30
+  Faina   20   10
+  Ulei    20   40
+  Faina   50   10
+
+Se afiseaza:
+ a)
+  Ulei    10   40  400
+  Zahar   5    30  150
+  Faina   20   10  200
+  Ulei    20   40  800
+  Faina   50   10  500
+
+b) ulei    30
+   zahar   5
+   faina   70
+
 */
 
-struct depozit{
-    long cod;
-    int cantitate, pret;
-    char denumire[41]; 
+struct depozit
+{
+    char nume_produs [21];
+    int cantitate;
+    int pret;
 };
+ depozit v[101];
 
-depozit v[1001];
-int n,nr;
+int n;
+int numar;
 
-void citire(){
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>v[i].cod;
-        cin>>v[i].cantitate;
-        cin>>v[i].pret;
-        cin.get();
-        cin.get(v[i].denumire,41);
+void citire ()
+{
+       cin >> n;
+    for (int i=1; i <=n; i++) 
+    
+{
+    cin.get (v[i].nume_produs, 21);
+         cin.get ();
+         cin >> v[i].cantitate;
+         cin >> v[i].pret;
+        
+}
+}
+
+void cantitate(int &nr, depozit v[101], int n)
+{
+    int nr = 0;
+
+for(int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            if ((v[i].nume_produs, 21) == (v[j].nume_produs, 21))
+                nr = v[i].cantitate + v[j].cantitate;
     }
 }
 
-void valTot(int &nr, depozit v[1001], int n){
-    nr=0;
-    for(int j=0;j<n;j++)
-        nr+=(v[j].pret*v[j].cantitate);
-}
-
-int main(){
+int main()
+{
     citire();
-    valTot(nr,v,n);
-    cout<<"Valoarea totala = "<<nr;
-    return 0;
+    cantitate(nr, v, n);
+    cout << "cantiate totale = "<< nr;
+     return 0;
 }
