5e66cf315927a113196faa7c35e8fb0edc4a7683
thecyberbro <thecyberbro@gmail.com>
2021-03-08 22:11:17 +0200

    ✅️

--- C++/ex.cpp
+++ C++/ex.cpp
@@ -1,45 +1,86 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-/*
-Acest program afla suma cirfelor dintr-un nr
-*/
 
-long x;
+//declarare
+int a, b, c, x1, x2;
 
 void citire()
 {
-    cin >> x;
+    cin >> a >> b >> c;
 }
 
-int sumCif(long x)
+void aflarex1x2(int a, int b, int c)
 {
-    int s = 0, f;
-
-    while (x)
+    //aflam delta
+    int delta = b * b - 4 * a * c;
+    if (delta < 0)
     {
-        f = x % 10;
-        s += f;
-        x /= 10;
+        cout << "Ecuatia nu are solutie în Z \n";
     }
-    return s;
+    else
+    { //aflam x1 si x2
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+    }
 }
 
-int sumCifRec(long n)
+int aria(int a, int b, int c)
 {
-    if (n == 0)
-        return 0;
-    else
-        return sumCifRec(n / 10) + n % 10;
+    int s = (a + b + c) / 2;
+    //formula lui heron
+    return sqrt(s * (s - a) * (s - b) * (s - c));
 }
+
+void checkTriunghi(int a, int b, int c)
+{
+    if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a)
+    {
+        // p = (a + b + c) / 2;
+        // cout << sqrt(p * (p - a) * (p - b) * (p - c)) << endl;
+        if (a == b && b == c)
+        {
+            cout << "echilateral";
+            cout << "\nAria este: " << aria(a, b, c);
+        }
+        if (a == b || b == c || a == c)
+        {
+            if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == b * b + a * a)
+            {
+                cout << "dreptunghic isoscel";
+                cout << "\nAria este: " << aria(a, b, c);
+            }
+            else
+            {
+                cout << "isoscel";
+                cout << "\nAria este: " << aria(a, b, c);
+            }
+        }
+        if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == b * b + a * a)
+        {
+            cout << "dreptunghic";
+            cout << "\nAria este: " << aria(a, b, c);
+        }
+        else
+        {
+            cout << "arbitrar";
+
+            cout << "\nAria este: " << aria(a, b, c);
+        }
+    }
+    else
+    {
+        cout << "datele introduse nu pot forma un triunghi\n";
+    }
+}
+
 int main()
-{ 
+{
     //se citeste nr
     citire();
-    //func iterativa
-    cout << sumCif(x);
-    //func recursiva
-    cout << sumCifRec(x);
+    aflarex1x2(a, b, c);
+    checkTriunghi(a, b, c);
 
     return 0;
 }
--- C++/main.cpp
+++ C++/main.cpp
@@ -7,75 +7,12 @@
 
 using namespace std;
 
-/*
-La un depozit de materiale exista n produse.
- Pentru fiecare produs se cunoaste:
-  nume_produs, cantitate, pret.
-Se cere: a)valoarea pentru fiecare produs
-         b)cantitatea pe tipuri de produse
+//recursive problem
+//c++ algoritms and data structures sucks
 
-Ex: n=5
-  Ulei    10   40
-  Zahar   5    30
-  Faina   20   10
-  Ulei    20   40
-  Faina   50   10
 
-Se afiseaza:
- a)
-  Ulei    10   40  400
-  Zahar   5    30  150
-  Faina   20   10  200
-  Ulei    20   40  800
-  Faina   50   10  500
-
-b) ulei    30
-   zahar   5
-   faina   70
-
-*/
-
-struct depozit
+int main()
 {
-    char nume_produs [21];
-    int cantitate;
-    int pret;
-};
- depozit v[101];
-
-int n;
-int numar;
-
-void citire ()
-{
-       cin >> n;
-    for (int i=1; i <=n; i++) 
-    
-{
-    cin.get (v[i].nume_produs, 21);
-         cin.get ();
-         cin >> v[i].cantitate;
-         cin >> v[i].pret;
-        
-}
-}
-
-void cantitate(int &nr, depozit v[101], int n)
-{
-    int nr = 0;
-
-for(int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-            if ((v[i].nume_produs, 21) == (v[j].nume_produs, 21))
-                nr = v[i].cantitate + v[j].cantitate;
-    }
-}
-
-int main()
-{
-    citire();
-    cantitate(nr, v, n);
-    cout << "cantiate totale = "<< nr;
-     return 0;
+    //TODO
+    return 0;
 }
