33c638bca68a05e57550546bb72d58589e2b8f37
theCYBERbro <56652297+theCYBERbro@users.noreply.github.com>
2021-03-23 20:12:34 +0200

    ✅️added some code

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,15 +2,23 @@
 //
 using namespace std;
 //
-
+bool multiplu(){
+return (a[i]%k==0 && a[i]%10==k)?true:false;
+}
+int multiplu(int a[] , int n , int k)
+{
+    int f=0;
+    for(int i = 0 ; i < n ; ++i)
+    {
+        if(multiplu())
+            f++;
+    }
+    return f;
+}
 int main()
 {
-    /*
     citire();
-    sortare();
-    afisare();
-    */
-
-   
+    P(n,x[101],s);
+    cout<<s;
     return 0;
 }
