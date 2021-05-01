a8e2db39c1d038561d1220e705105f1fe250ce76
theCYBERbro <56652297+theCYBERbro@users.noreply.github.com>
2021-05-01 17:55:51 +0300

    added files part 2

--- C++/TemaVacanta/200-201(21)/1.cpp
+++ C++/TemaVacanta/200-201(21)/1.cpp
@@ -2,19 +2,25 @@
 //
 using namespace std;
 //
-
+//program recursiv de inmultire a 2 numere
+int a, b;
 void citire()
 {
+    cin>>a>>b;
 }
 
-void prelucrare()
+void prelucrare(int a,int b,int k)
 {
+    if(k<a){
+        return prelucrare(a,b,k++);
+        
+    }
 }
 
 int main()
 {
-
+int k=0;
     citire();
-    prelucrare();
+    prelucrare(a,b,k);
     return 0;
 }
