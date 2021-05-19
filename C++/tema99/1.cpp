5e0b06fdb208b691680cba189e7523194df181f4
thecyberbro <thecyberbro@gmail.com>
2021-05-19 11:39:07 +0300

    code🤡

---
+++ C++/tema99/1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+//
+using namespace std;
+
+//1
+int suma(int a, int b){
+    return a+b;
+}
+
+//2
+
+//3
+int cmmv(int a,int b){
+    return (a<b)?b:a;
+}
+
+//4
+int valMedVec(int a[], int nr){
+    int s;
+    for (int i = 0; i <=nr; i++)
+    {
+        s+=a[i];
+    }
+    return s/nr;
+}
+
+int main()
+{
+    return 0;
+}
