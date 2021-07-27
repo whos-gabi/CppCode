da72872cdae548dd1dec1626fe9e737f3db16102
thecyberbro <thecyberbro@gmail.com>
2021-07-27 18:09:05 +0300

    🤓 probleme vacanta 1

--- C++/lista_dublu_lantuita.cpp
+++ C++/lista_dublu_lantuita.cpp
@@ -1,3 +1,4 @@
+//lista dublu lantuita
 #include <iostream>
 
 using namespace std;
@@ -7,8 +8,8 @@ struct nod
     nod *urm, *ant;
 };
 
-nod *p, *u, *prim, *ultim;
-int n;
+nod *p, *u, *prim, *ultim, *q;
+int n, x;
 
 void creare()
 {
@@ -31,6 +32,44 @@ void creare()
     u->urm = NULL;
 }
 
+void stergere()
+{
+    p = prim;
+    cout << "nodul spre stergere: ";
+    cin >> x;
+    if (p->info == x)
+    {
+        q = p;
+        p = p->urm;
+        prim = p;
+        delete (q);
+    }else{
+        while(p->urm->info!=0){
+            p=p->urm;
+        }
+        q=p->urm;
+        p->urm=q->urm;
+        q->urm->ant=p;
+        delete(q);
+    }
+}
+
+void inserare(){
+    p=prim;
+    cout<<"Dati nodul dupa care se face inserarea: ";
+    cin>>x;
+    while(p->info!=x){
+        p=p->urm;
+    }
+    q= new nod;
+    cout<<"Dati informatia noului nod: ";
+    cin>>q->info;
+    q->urm=p->urm;
+    p->urm->ant=q;
+    p->urm=q;
+    q->ant=p;
+}
+
 void afisare_sd()
 {
     p = prim;
@@ -41,20 +80,25 @@ void afisare_sd()
     }
 }
 
-void afisare_ds(){
-       p = u->ant;
+void afisare_ds()
+{
+    p = u->ant;
     while (p != NULL)
     {
         cout << p->info << " ";
         p = p->ant;
-    } 
+    }
 }
 
 int main()
 {
     creare();
 
-    afisare_ds();
-    cout<<"\n";
+    // afisare_ds();
+    // cout << "\n";
     afisare_sd();
+    inserare();
+    cout<<'\n';
+    afisare_sd();
+
 }
---
+++ C++/probleme_vacanta/2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//prod cif impare intrun numar
+
+int nr, p = 1;
+
+void citire()
+{
+    cin >> nr;
+}
+
+int imPar(int k)
+{
+    return k % 2 != 0;
+}
+
+int prelucrare(int nr, int &p)
+{
+    int n = nr, uc, z;
+    while (n != 0)
+    {
+        uc = n % 10;
+        if (imPar(uc))
+        {
+            p *= uc;
+            z++;
+        }
+        n /= 10;
+    }
+    if (z != 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return p;
+    }
+}
+
+int main()
+{
+    citire();
+    cout << prelucrare(nr, p);
+
+    return 0;
+}
